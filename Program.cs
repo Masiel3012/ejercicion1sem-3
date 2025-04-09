89b69d2cb41bf650b494054da2180e8b84c90ac5
Tony <andre19@gmail.com>
2025-04-08 19:09:28 -0500

    semana_3 v.1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ namespace ejercicion1sem_3
     {
         static void Main(string[] args)
         {
-            
+           int n1, n2, suma;                                       //llamar variables (en reserva)
+            Console.Write("Ingrese numero 1:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese numero 2:");
+            // es igual a leer a pseint
+            n2 = int.Parse(Console.ReadLine());
+            suma = n1 + n2;
+            Console.WriteLine("la suma es :"+suma);              //une y concatena el texto con el reultado
+            Console.ReadKey();
         }
+        
+
+
     }
+
 }
